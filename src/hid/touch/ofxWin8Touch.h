f93fdb0cfb9fb1b04625298ead974d128be5a082
Lolo <lolo@armdz.com>
2019-04-01 12:38:36 -0300

    added touch

---
+++ src/hid/touch/ofxWin8Touch.h
@@ -0,0 +1,14 @@
+/*
+ * ofxWin8Touch.h
+ * Header file for Windows 8 touch event handling.
+ *
+ * Created by Robert Xiao on August 24, 2015.
+ */
+
+#pragma once
+
+#ifdef TARGET_WIN32
+
+void ofxWin8TouchSetup();
+
+#endif
